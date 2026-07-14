cb7e92744ce3bfe629603c7289da78b5e51d2a56
Pushill <minseom45@gmail.com>
2026-07-14 16:13:23 +0900

    FirstStageSound.cs

--- FirstStageSound.cs
+++ FirstStageSound.cs
@@ -1,8 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: FirstStageSound
-// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 40EE2C12-5186-4819-9200-6B792B4A111D
-// Assembly location: C:\Users\minse\Downloads\Pale to Vivid(2026학년도 1학기 개인프로젝트)\Pale2Vivid\2026_1st_solo_project_Data\Managed\Assembly-CSharp.dll
+
 
 using System.Collections;
 using UnityEngine;
